da23501b421144ffb09be04b01b7030e651edee5
Raj101145 <rpurohit573@gmail.com>
2026-02-14 03:05:45 +0530

    stored data in mongodb and used data validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,20 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
-
+const {body,validationResult} = require("express-validator");
 // Create User
-router.post("/createuser", async (req, res) => {
-    try {
-        const user = await User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        });
+router.post(
+  "/createuser",  
+  [
+    body("name", "Name must be at least 3 characters").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be at least 5 characters").isLength({ min: 5 })
+  ],
+  async (req, res) => {
 
-        res.json(user);
-    } catch (error) {
-        res.status(500).send("Server Error");
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-});
 
+    try {
+      const user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+
+      res.json(user);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 module.exports = router;
